470b8ca2aae501282d52b2c0ea9443ada447ad09
Tiago Minuzzi <tiago.minuzzi87@gmail.com>
2020-05-08 16:24:25 -0300

    Debian pos-install altered

--- debian/debian_pos_install.sh
+++ debian/debian_pos_install.sh
@@ -3,7 +3,7 @@
 ##############################
 
 # System utilities
-sudo apt install gnome-tweak-tool vim vlc buil-essential git ssh openssh-server make flameshot chromium chrome-gnome-shell libappindicator3-1 terminator python3 python3-pip htop firmware-linux-nonfree plymouth plymouth-themes redshift redshift-gtk caffeine -y
+sudo apt install gnome-tweak-tool vim vlc buil-essential git ssh openssh-server make flameshot chromium chrome-gnome-shell libappindicator3-1 terminator python3 python3-pip htop firmware-linux-nonfree plymouth plymouth-themes redshift redshift-gtk caffeine font-fonts-awesome neofetch -y
 
 # Flash-plugin
 sudo apt install pepperflashplugin-nonfree browser-plugin-freshplayer-pepperflash -y
