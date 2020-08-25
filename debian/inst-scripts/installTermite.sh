c2435abda3272b5954d43a5a353c82edb02f0a11
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2020-08-24 22:38:31 -0300

    libtool added to termite install script

--- debian/inst-scripts/installTermite.sh
+++ debian/inst-scripts/installTermite.sh
@@ -3,7 +3,7 @@
 # Install dev tools
 sudo apt -y build-essential
 # Install build dependencies
-sudo apt install -y git g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf
+sudo apt install -y git g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf libtool
 # Install vte
 git clone https://github.com/thestinger/vte-ng.git
 echo export LIBRARY_PATH="/usr/include/gtk-3.0:$LIBRARY_PATH"
