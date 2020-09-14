ac63b984b9fdcaf4ff7cb88e81f76a14d34d8e45
Tiago Minuzzi <tiago@localhost.localdomain>
2020-09-14 15:24:48 -0300

    Fedora and flatpak related files

---
+++ fedora/installPacks.sh
@@ -0,0 +1,4 @@
+# Common programs
+sudo dnf install vim vim-airline ufw transmission-gtk tilix flameshot neofetch htop gnome-tweaks chromium timeshift -y 
+# Dev tools
+sudo dnf install gcc gcc-c++ kernel-devel make cmake automake zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel -y
