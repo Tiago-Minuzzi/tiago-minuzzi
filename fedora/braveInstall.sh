eee4f96d42d4b2d4dc888ffa4c653016b29d771f
Tiago-Minuzzi <tiago.minuzzi87@google.com>
2020-11-02 20:44:43 -0300

    Brave browser install script

---
+++ fedora/braveInstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo dnf install dnf-plugins-core -y &&
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ &&
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc &&
+sudo dnf install brave-browser -y
