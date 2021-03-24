df23362058d869ef47bf65c0bb776b3ada2a3c35
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-03-24 08:54:16 -0300

    earlyoom added

--- arch_linux/installMyPacks.sh
+++ arch_linux/installMyPacks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # My packs
-sudo pacman -S archlinux-wallpaper awesome-terminal-fonts chrome-gnome-shell deja-dup firefox flameshot flatpak flex gimp go libreoffice-fresh materia-gtk-theme neofetch pdfgrep perl-font-ttf perl-rename pkgconf ps_mem terminus-font systemd-swap tilix tlp transmission-gtk ttf-hack ttf-nerd-fonts-symbols-mono ufw vim-airline vlc youtube-dl --noconfirm
+sudo pacman -S archlinux-wallpaper awesome-terminal-fonts chrome-gnome-shell deja-dup earlyoom firefox flameshot flatpak gimp libreoffice-fresh neofetch pdfgrep perl-font-ttf perl-rename pkgconf ps_mem terminus-font tilix tlp transmission-gtk ttf-fira-code ttf-hack ttf-nerd-fonts-symbols-mono ttf-roboto ttf-roboto-mono ufw vim-airline mpv youtube-dl --noconfirm
