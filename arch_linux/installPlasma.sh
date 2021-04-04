51f3de63218168fd33b4480e6de9a431a0be6800
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-04-04 18:58:03 -0300

    Packages needed for installing global themes

--- arch_linux/installPlasma.sh
+++ arch_linux/installPlasma.sh
@@ -1,1 +1,1 @@
-sudo pacman -S plasma-desktop kde-gtk-config kmix kscreen kdeplasma-addons kio kdegraphics-thumbnailers plasma-nm plasma-pa
+sudo pacman -S plasma-desktop kde-gtk-config kmix kscreen kdeplasma-addons kio kdegraphics-thumbnailers plasma-nm plasma-pa sddm sddm-kcm
