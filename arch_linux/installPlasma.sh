985826d445c088f5a6af707e7c3229454ba9d741
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-04-17 16:03:48 -0300

    Install konsole

--- arch_linux/installPlasma.sh
+++ arch_linux/installPlasma.sh
@@ -1,1 +1,1 @@
-sudo pacman -S plasma-desktop kde-gtk-config kmix kscreen kdeplasma-addons kio kdegraphics-thumbnailers plasma-nm plasma-pa sddm sddm-kcm
+sudo pacman -S plasma-desktop kde-gtk-config kmix kscreen kdeplasma-addons kio kdegraphics-thumbnailers plasma-nm plasma-pa sddm sddm-kcm konsole
