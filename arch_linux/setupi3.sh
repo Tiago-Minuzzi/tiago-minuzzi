7c84c0047a3f25bc0a5cf8c1528ea60dcfbedc98
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-04-22 13:01:45 -0300

    i3 setup for Arch Linux

---
+++ arch_linux/setupi3.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Install essentials and tools
+sudo pacman -Sy i3-gaps \
+               i3status \
+               i3lock \
+               feh \
+               redshift \
+               lxappearance \
+               lxsession \
+               picom \
+               dunst --noconfirm
+# Install pkgs from AUR
+yay -S clipit \
+       caffeine-ng --noconfirm
+# Enable necessary services for redshift
+sudo systemctl enable --now avahi-daemon.service &&\
+sudo systemctl enable --now geoclue.service
