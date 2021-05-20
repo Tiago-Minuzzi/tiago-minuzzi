6cbac07047dde7788b5fea77756cb75fff46fcda
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-05-20 09:05:49 -0300

    Install xfce4

---
+++ arch_linux/installXfce4.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo pacman -S xfce4 xfce4-battery-plugin xfce4-cpufreq-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-sensors-plugin xfce4-weather-plugin thunar-media-tags-plugin thunar-archive-plugin xfce4-artwork xfce4-battery-plugin xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin mousepad
