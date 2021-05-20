3745ca4ee49875d19cd11e843209f9cbb2ef52a5
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-05-20 09:18:08 -0300

    Install pcmanfm-gtk3 & minor changes

--- arch_linux/setupi3.sh
+++ arch_linux/setupi3.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 # Install essentials and tools
 sudo pacman -Sy i3-gaps \
-               i3status \
-               i3lock \
-               feh \
-               redshift \
-               lxappearance \
-               lxsession \
-               picom \
-               playerctl \
-               dunst --noconfirm
+                i3status \
+                i3lock \
+                feh \
+                geoclue \
+                redshift \
+                lxappearance-gtk3 \
+                lxsession-gtk3 \
+                playerctl \
+                unclutter \
+                pcmanfm-gtk3 \
+                ranger \
+                dunst --noconfirm
 # Install pkgs from AUR
 yay -S clipit \
+       picom-ibhagwan-git \
        caffeine-ng --noconfirm
 # Enable necessary services for redshift
 sudo systemctl enable --now avahi-daemon.service &&\
