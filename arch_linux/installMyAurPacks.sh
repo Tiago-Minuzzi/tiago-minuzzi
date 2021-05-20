f2865365be42b62dae2470d336321b723b788e88
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-05-20 09:24:16 -0300

    Zramd & new packs

---
+++ arch_linux/installMyAurPacks.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# My aur packs
+yay -S megax-bin rstudio-desktop-preview-bin masterpdfeditor timeshift ttf-ms-fonts python-pulsectl zramd brave-bin --noconfirm
--- arch_linux/installMyYayPacks.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# My aur packs
-yay -S megax-bin rstudio-desktop-preview-bin masterpdfeditor timeshift --noconfirm
