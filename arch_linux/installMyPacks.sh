700f93f90bab1f1597ea2f88f7b4a462bb23d7bf
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-05-25 17:52:22 -0300

    +Alacritty;-tilix

--- arch_linux/installMyPacks.sh
+++ arch_linux/installMyPacks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # My packs
-sudo pacman -S archlinux-wallpaper awesome-terminal-fonts chrome-gnome-shell deja-dup earlyoom firefox flameshot flatpak gimp libreoffice-fresh neofetch pdfgrep perl-font-ttf perl-rename pkgconf ps_mem terminus-font tilix tlp transmission-gtk ttf-fira-code ttf-hack ttf-nerd-fonts-symbols-mono ttf-roboto ttf-roboto-mono ttf-ubuntu-font-family ttf-dejavu ttf-bitstream-vera ttf-liberation ttf-font-awesome otf-font-awesome noto-fonts ufw vim-airline mpv youtube-dl --noconfirm
+sudo pacman -S alacritty archlinux-wallpaper awesome-terminal-fonts deja-dup earlyoom firefox flameshot flatpak gimp libreoffice-fresh neofetch pdfgrep perl-font-ttf perl-rename pkgconf ps_mem terminus-font tlp transmission-gtk ttf-fira-code ttf-hack ttf-nerd-fonts-symbols-mono ttf-roboto ttf-roboto-mono ttf-ubuntu-font-family ttf-dejavu ttf-bitstream-vera ttf-liberation ttf-font-awesome otf-font-awesome noto-fonts ufw vim-airline mpv youtube-dl --noconfirm
