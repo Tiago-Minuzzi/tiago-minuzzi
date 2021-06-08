6b14ebd9ca91d06f0b3ee5b4d23a4ebba3b7fa6b
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-06-08 10:30:56 -0300

    xfce terminal floating

--- dwm/config.h
+++ dwm/config.h
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Xfce4-terminal",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
