26b7ef08f0f3262f47019ba3272bbe8feff90425
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-06-08 10:47:01 -0300

    autostart path etc

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1393,7 +1393,7 @@ run(void)
 void
 runAutostart(void) {
 	system("cd ~/.dwm; ./autostart_blocking.sh");
-	system("cd ~/.config/dwm; ./autostart.sh &");
+	system("cd ~/repos/tiago-minuzzi/dwm; ./autostart.sh &");
 }
 
 void
