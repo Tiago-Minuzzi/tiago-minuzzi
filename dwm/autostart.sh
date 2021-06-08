4d22d8c8158546f1072ef1aac64f48a6355c5465
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-06-08 14:29:58 -0300

    Refresh weather

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,14 +1,19 @@
-TEMPO=$(curl wttr.in?format=1)
+while true
+do
+    curl "wttr.in?format=1" > /tmp/currWeather.tmp
+    sleep 1h
+done &
 
 while true
 do
-CPU=$(top -bn1 | grep "Cpu(s)" | \
+    TEMPO=$(< /tmp/currWeather.tmp)
+    CPU=$(top -bn1 | grep "Cpu(s)" | \
       sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
       awk '{printf("%02d%\n",100 - $1)}')
-DISCO=$(df -h /dev/sda1 | awk 'NR==2{print $3}')
-MEM=$(free -h | awk '(NR==2){ print $3 }')
-DATA=$(date '+%A%e %B')
-HORA=$(date +%H:%M)
+    DISCO=$(df -h /dev/sda1 | awk 'NR==2{print $3}')
+    MEM=$(free -h | awk '(NR==2){ print $3 }')
+    DATA=$(date '+%A%e %B')
+    HORA=$(date +%H:%M)
     xsetroot -name " ${TEMPO} | ${CPU} | ${DISCO} | ${MEM} | ${DATA} | ${HORA} "
-    sleep 1
-done
+    sleep 2s
+done &
