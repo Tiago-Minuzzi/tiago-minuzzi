94a80dab152440462e8841680278a41dcdff80b4
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-06-20 14:02:20 -0300

    Apps to tags

--- dwm/config.h
+++ dwm/config.h
@@ -31,7 +31,16 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Xfce4-terminal",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "Alacritty",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Pcmanfm",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Virt-manager",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Spotify",  NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Brave-browser",  NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "qutebrowser",  NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Zathura",  NULL,       NULL,       1 << 6,       0,           -1 },
+	{ "libreoffice-writer",  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "libreoffice-calc",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
