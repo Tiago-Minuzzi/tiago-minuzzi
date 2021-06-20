633d0bccc70c85a578c848f51c18ea7201a0bb6b
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-06-19 21:49:54 -0300

    Slstatus values padded

--- slstatus/config.h
+++ slstatus/config.h
@@ -65,9 +65,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ cpu_perc, " CPU %s%% | ",     NULL },
-	{ ram_perc, "RAM %s%% | ",     NULL },
-	{ swap_perc, "SWP %s%% | ",     NULL },
-	{ disk_perc, "DSK %s%% | ",     "/" },
+	{ cpu_perc, " CPU %2s%% | ",     NULL },
+	{ ram_perc, "RAM %2s%% | ",     NULL },
+	{ swap_perc, "SWP %2s%% | ",     NULL },
+	{ disk_perc, "DSK %2s%% | ",     "/" },
 	{ datetime, "%s",           "%F | %H:%M" },
 };
