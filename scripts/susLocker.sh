c82bd6ea5c00b03d112fe49b7ea2dd3708cf239f
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-06-20 11:18:01 -0300

    Logout dwm kb; locker & weather

---
+++ scripts/susLocker.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+i3lock --color 000000 && xset dpms force off && sync && systemctl suspend
---
+++ scripts/weather.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+while true
+do
+    curl "wttr.in?format=1"
+    sleep 1800s
+done &
