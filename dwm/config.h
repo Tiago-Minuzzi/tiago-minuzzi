cd6cf671f6f181328c3caa999bf9714cd87cfaaf
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-06-20 14:33:27 -0300

    Tag icons changed

--- dwm/config.h
+++ dwm/config.h
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "爵", "龎", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
