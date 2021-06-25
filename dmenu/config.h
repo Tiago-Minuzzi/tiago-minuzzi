b6efffdb290a6fbab83d4cc689498f0086bfce95
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-06-25 07:15:48 -0300

    Green accent

--- dmenu/config.h
+++ dmenu/config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#212121" },
-	[SchemeSel] = { "#eeeeee", "#5a85ff" },
+	[SchemeSel] = { "#eeeeee", "#279881" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
