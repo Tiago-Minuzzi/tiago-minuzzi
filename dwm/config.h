0db073232c571ffd4ad483c50db3cec41ae587cb
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-07-13 14:58:31 -0300

    Bar at top

--- dwm/config.h
+++ dwm/config.h
@@ -6,9 +6,9 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Noto Sans Mono:style=Bold:size=11","Font Awesome 5 Free Solid:size=10" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char dmenufont[]       = "Noto Sans Mono:style=Bold:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
