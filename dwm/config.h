7e0da70e02ad4ca0905fa07b6f7144580fd4ae7b
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-07-19 08:53:22 -0300

    Less tags

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Noto Sans Mono:style=Bold:size=11","Font Awesome 5 Free Solid:size=10" };
 static const char dmenufont[]       = "Noto Sans Mono:style=Bold:size=11";
 static const char col_gray1[]       = "#171a1f";
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "爵", "龎", "", "" };
+static const char *tags[] = { "", "", "", "", "", "爵", "龎" };
 
 static const Rule rules[] = {
 	/* xprop(1):
