3fccb20523812364e33532b7189facda22079c23
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2022-01-10 21:04:51 -0300

    Weather script updated

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-while true
-do
-    curl "wttr.in?format=1"
-    sleep 1800s
-done &
+#while true
+#do
+#    curl "wttr.in?format=1"
+#    sleep 1800s
+#done
+curl "wttr.in?format=1" > /tmp/currWeather.txt
