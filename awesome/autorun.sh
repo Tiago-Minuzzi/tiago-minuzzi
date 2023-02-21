ef13d77bd0d5703adaa4572563854e86109ca56e
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-02-21 17:39:38 -0300

    Avoid blank screen

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -11,3 +11,4 @@ run picom --config /home/tiago/.config/picom/picom.conf
 run setxkbmap -layout us -variant intl
 run sxhkd -c /home/tiago/.config/sxhkd/sxhkdrc
 run ~/.screenlayout/awesome.sh
+xset dpms 0 0 0 && xset s noblank  && xset s off
