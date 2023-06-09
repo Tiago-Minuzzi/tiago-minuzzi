a9bdb1d08e45b4bd33b5c4c14f3d8033c35f14c0
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-06-09 18:07:06 -0300

    awesome kbd launcher changed to 'd' from 'r'

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -309,7 +309,7 @@ globalkeys = gears.table.join(
 --              {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
+    awful.key({ modkey },            "d",     function () awful.screen.focused().mypromptbox:run() end,
               {description = "run prompt", group = "launcher"}),
 
     awful.key({ modkey }, "x",
