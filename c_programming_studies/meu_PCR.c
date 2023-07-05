5b2fd6f462f6fabf94b9a1495b8f8c6c552d8209
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-07-05 14:57:35 -0300

    PCR simulation in C

--- c_programming_studies/meu_PCR.c
+++ c_programming_studies/meu_PCR.c
@@ -1,42 +1,92 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <unistd.h>
 
-int limiteDisparo1 = 45;
-int limiteDisparo2 = 30;
+int tolerancia = 1;
 
-float temperature = 40.2; // calcula a temperatura
+bool halter = false;
+bool halter_ciclo = false;
 
+int ti = 85;
 
-void step01() {
-    printf(">>> Step 01\n");
-    printf("Disparo = %d\n",limiteDisparo1);
-    printf("Temperatura: %.2f °C\n",temperature);
+int temp_des = 94;
+int temp_ane = 55;
+int temp_ext = 72;
+int temp_man = 4;
 
-    if (temperature < limiteDisparo1) {
-        printf("Baixo\n\n");
-    } else {
-        printf("Alto\n\n");
-    }
-    sleep(2);
+int controlador(int target_temp, int *t0, int timef) {
+    
+    if ( *t0 == target_temp ) {
+        int time0 = 1;
+        if (timef == -1) {
+            time0 = 0;
+        }
+        printf("Mantendo. T: %d\n",*t0); 
+        do { 
+            printf("Tempo: %d\n",time0); 
+            if (time0 == timef) {
+                return 1; }
+            time0++; 
+            usleep(.5 * 1000000); } while (time0 <= timef); }
+    else if ( *t0 <= (target_temp - tolerancia) ) { 
+        printf("Aumentando. T: %d\n",*t0);
+        (*t0) += 1; 
+        usleep(.3 * 1000000); }
+    else if ( *t0 >= (target_temp + tolerancia) ) {
+        printf("Diminuindo. T: %d\n",*t0);
+        (*t0) -= 1; 
+        usleep(.3 * 1000000); }
+
+    return 0;
+
 }
 
-void step02() {
-    printf(">>> Step 02\n");
-    printf("Disparo = %d\n",limiteDisparo2);
-    printf("Temperatura: %.2f °C\n",temperature);
+void main() {
 
-    if (temperature > limiteDisparo2) {
-        printf("Alto\n\n");
-    } else {
-        printf("Baixo\n\n");
+    bool verdadeiro = true;
+    
+    if (halter == false) {
+        printf("### Rodando etapa incial\n");
     }
-    sleep(2);
-}
 
-void main(){
-    printf("### Temperatura setada em %.2f ###\n\n",temperature);
-    step01();
-    step02();
-    sleep(3);
-    printf("Fim\n");
+    int ci = 1;
+    int ciclos = 3;
+
+    while (verdadeiro) {
+        // ++++++++++++++++++++++++++++++++++++++++++++++++
+        
+
+        if (halter == false) {
+            int f0 = controlador(temp_des, &ti, 5);
+            if (f0 == 1) {
+                halter = true; 
+            } 
+
+        } else if (halter_ciclo == false) {
+
+            while(ci <= ciclos){
+                printf(">>> Ciclo %d\n",ci);
+                printf("--- Denaturation step---\n");
+                while(controlador(temp_des,&ti, 7) == 0){}
+                printf("--- Annealing step---\n");
+                while(controlador(temp_ane,&ti, 10) == 0){}
+                printf("--- Extension step---\n");
+                while(controlador(temp_ext,&ti, 5) == 0){}
+                ci++;
+            } 
+
+
+            if ( ci > ciclos) {
+                halter_ciclo = true;
+                printf("### Finalizando\n");
+            }
+
+        } else { 
+            controlador(temp_man, &ti, -1);
+            }
+
+
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++
+    }
 }
