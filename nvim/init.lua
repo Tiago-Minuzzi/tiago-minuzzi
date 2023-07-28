4cb12ef9cc825da9485c458a16796c5517429d96
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-07-28 07:25:51 -0300

    Clangd lsp set up; Mappings for lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,12 +3,21 @@ vim.cmd('source $HOME/.config/nvim/vim-plug/plugins.vim')
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("lspconfig")["pylsp"].setup{}
+require("lspconfig")["clangd"].setup{}
+
+-- Set up LSP key mappings for pylsp
+-- Example: Go to definition
+vim.api.nvim_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+
+-- Set up LSP key mappings for clangd
+-- Example: Find references
+vim.api.nvim_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
 
 -- Set leader key
 vim.g.mapleader = ' '
 
 -- Save and continue on file
-vim.api.nvim_set_keymap('n', 'zz', ":update<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', 'zz', ":update<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('v', '<C-J>', ":m '>+1<CR>gv=gv", { noremap = true })
 vim.api.nvim_set_keymap('v', '<C-K>', ":m '<-2<CR>gv=gv", { noremap = true })
@@ -21,8 +30,7 @@ vim.api.nvim_set_keymap('n', '<leader>yy', '"+yy', { noremap = true })
 -- Paste from clipboard
 vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true })
-vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true })
-vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
+vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true }) vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
 
 vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
