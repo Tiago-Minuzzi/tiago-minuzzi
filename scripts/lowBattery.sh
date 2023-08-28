f0b3375221e938721499db437fe1ee557139cf9b
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-08-28 10:29:51 -0300

    Comment on how to run the lowBattery.sh script

--- scripts/lowBattery.sh
+++ scripts/lowBattery.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# To use this script
+# Create a cronservice with the command
+# crontab -e (may need to specify the editor: EDITOR=nvim crontab -e)
+# and add
+# */5 * * * * /home/tiago/.local/bin/lowBattery.sh (it sets to check battery every 5 min)
 
 battery_level=$(cat /sys/class/power_supply/BAT1/capacity)
 low_threshold=20
