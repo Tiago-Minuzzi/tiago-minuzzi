f6a71aa301e9f795f5ab3ced43adae4d87a31d78
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2024-08-28 09:00:19 -0300

    Read from stdin

--- c_programming_studies/fq_counter.c
+++ c_programming_studies/fq_counter.c
@@ -8,32 +8,36 @@ int main(int argc, char **argv){
     FILE *f;
     char linha[MAX_LINE_LENGTH];
 
-    if (argc != 2) {
+    int indice = 0;
+    int total_bases = 0;
+    int total_linhas = 0;
+    size_t read_len = 0;
+
+    if (argc > 2) {
         printf(">>> Error reading file.\n");
         printf("    Usage: %s FILE\n", argv[0]);
         exit(1);
-    } else {
-        int indice = 0;
-        int total_bases = 0;
-        int total_linhas = 0;
-
-        size_t read_len = 0;
-
-        f = fopen(argv[1], "r");
-
-        while (fgets(linha, MAX_LINE_LENGTH, f)){
-            indice++;
-            if ((indice % 4) == 2) {
-                // printf("%s", linha);
-                // printf("%d\n", strlen(linha));
-                read_len = strlen(linha);
-                total_bases += read_len;
-                total_linhas++;
-            }
-        }
-        printf("%s\t%d\t%d\n", argv[1], total_linhas, total_bases);
     }
 
-    fclose(f);
+    if (argc == 2) {
+        f = fopen(argv[1], "r");
+    } else {
+        f = stdin;
+    }
+
+    while (fgets(linha, MAX_LINE_LENGTH, f)){
+        indice++;
+        if ((indice % 4) == 2) {
+            read_len = strlen(linha);
+            total_bases += read_len;
+            total_linhas++;
+        }
+    }
+    printf("%s\t%d\t%d\n", argv[1], total_linhas, total_bases);
+
+    if (argc == 2) {
+        fclose(f);
+    }
+
     return 0;
 }
