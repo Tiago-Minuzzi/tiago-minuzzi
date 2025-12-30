1adcd983762e45d6777a7a256b0f7d517dc1efe7
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2025-12-30 09:27:37 -0300

    Run fqToFA concurrently

--- go/fqToFa.go
+++ go/fqToFa.go
@@ -4,10 +4,17 @@ import (
   "os"
   "fmt"
   "log"
+  "sync"
   "bufio"
+  "runtime"
   "strings"
   "path/filepath"
 )
+
+type Record struct {
+  ID  []byte
+  Seq []byte
+}
 
 func main() {
 
@@ -17,8 +24,8 @@ func main() {
     os.Exit(1)
   }
 
-  arquivo := os.Args[1]
-  entrada, err := os.Open(arquivo)
+  arquivo       := os.Args[1]
+  entrada, err  := os.Open(arquivo)
 
   if err != nil {
     log.Printf("\n\tErro na leitura: %v", err)
@@ -27,13 +34,10 @@ func main() {
 
   defer entrada.Close()
 
-  baseName := filepath.Base(arquivo)
+  baseName  := filepath.Base(arquivo)
   extension := filepath.Ext(baseName)
-  newName := strings.TrimSuffix(baseName, extension)
-  newName = fmt.Sprintf("%s.fasta", newName)
-
-  idx := 1
-
+  newName   := strings.TrimSuffix(baseName, extension)
+  newName    = fmt.Sprintf("%s.fasta", newName)
 
   saida, err := os.Create(newName)
 
@@ -44,29 +48,76 @@ func main() {
 
   defer saida.Close()
 
-  scanner := bufio.NewScanner(entrada)
-  buf := make([]byte, 64*1024)
-  scanner.Buffer(buf, 1024*1024)
-
   writer := bufio.NewWriterSize(saida, 1024*1024)
 	defer writer.Flush()
 
-  for scanner.Scan() {
-    linha := scanner.Bytes()
-
-    switch idx % 4 {
-    case 1:
-      writer.WriteByte('>')
-      if len(linha) > 0 {
-        writer.Write(linha[1:])
+  // Pool de memória para evitar alocações constantes
+  recordPool := sync.Pool {
+    New: func() interface{} {
+      return &Record {
+        ID:   make([]byte, 0, 512),
+        Seq:  make([]byte, 0, 1024),
       }
+    },
+  }
+
+  numWorkers := runtime.NumCPU()
+  jobs    := make(chan *Record, numWorkers * 2)
+  results := make(chan *Record, numWorkers * 2)
+  var wg sync.WaitGroup
+
+  for i:= 0; i < numWorkers; i++ {
+    wg.Add(1)
+    go func ()  {
+      defer wg.Done()
+      for r := range jobs {
+        if len(r.ID) > 0 {
+          r.ID[0] = '>'
+        }
+        results <- r
+      }
+    }()
+  }
+
+  doneWriting := make(chan bool)
+  go func() {
+    for r := range results {
+      writer.Write(r.ID)
+      writer.WriteByte('\n')
+      writer.Write(r.Seq)
       writer.WriteByte('\n')
 
+      r.ID  = r.ID[:0]
+      r.Seq = r.Seq[:0]
+      recordPool.Put(r)
+    }
+    doneWriting <- true
+  }()
+
+  scanner := bufio.NewScanner(entrada)
+
+  idx := 1
+
+  var currentRecord *Record
+
+  for scanner.Scan() {
+    
+    mod := idx % 4
+    switch mod {
+    case 1:
+      currentRecord = recordPool.Get().(*Record)
+      currentRecord.ID = append(currentRecord.ID, scanner.Bytes()...)
     case 2:
-      writer.Write(linha)
-      writer.WriteByte('\n')
+      currentRecord.Seq = append(currentRecord.Seq, scanner.Bytes()...)
+      jobs <- currentRecord
     }
     idx++
   }
+
+  close(jobs)
+  wg.Wait()
+  close(results)
+  <-doneWriting
+
   fmt.Println(">>> Programa finalizado!")
 }
