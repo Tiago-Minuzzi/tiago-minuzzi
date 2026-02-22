f2b27645c20892c202f344b1f0fb4db759ef7ae7
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2026-02-21 21:32:34 -0300

    Golang code to read TSV file. Also used struct and channels

---
+++ go/tsvChannelAndStruct.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"io"
+	"fmt"
+	"os"
+	"strconv"
+	"encoding/csv"
+)
+
+type CsvRecord struct {
+	ID string
+	Pos int
+	Saav string
+}
+
+func main() {
+	arquivo, err := os.Open("exemplo_vars.tsv")
+	if err != nil {
+		panic(err)
+	}
+	defer arquivo.Close()
+
+	records, _ := tabReader(arquivo)
+	for r := range records {
+		fmt.Printf("Campo 1: %v, Campo 2: %v, Campo 3: %v\n", r.ID, r.Pos, r.Saav)
+	}
+
+}
+
+func tabReader(f *os.File) (<-chan CsvRecord, <-chan error) {
+	outCh := make(chan CsvRecord)
+	errCh := make(chan error, 1)
+	
+	reader := csv.NewReader(f)
+	reader.Comma = '\t'
+
+	go func() {
+		defer close(outCh)
+		defer close(errCh)
+
+		for {
+			record, err := reader.Read()
+			if err == io.EOF {
+				return
+			}
+			
+			if err != nil {
+				errCh <- err
+				return
+			}
+			pos, err := strconv.Atoi(record[1])
+			if err != nil {
+				errCh <- err
+			}
+
+			
+			p := CsvRecord{
+				ID: record[0],
+				Pos: pos,
+				Saav: record[2],
+			}
+
+			outCh <- p
+		}
+	} ()
+	return outCh, errCh
+}
