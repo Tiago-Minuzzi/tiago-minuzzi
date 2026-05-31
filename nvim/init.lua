e3d0db11f4775bbbe472dfe3b5829e5f9ae87b7e
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2026-05-31 05:44:07 -0300

    New configs from scratch for neovim 0.12

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,71 +1,13 @@
--- (N)Vim plugins
-vim.cmd('source $HOME/.config/nvim/vim-plug/plugins.vim')
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("lspconfig")["pylsp"].setup{}
-require("lspconfig")["clangd"].setup{}
-require("lspconfig")["gopls"].setup{}
-require('Comment').setup()
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = 'yes:1'
+vim.o.confirm = true
 
--- load nvim-cmp
-local cmp = require('cmp')
-local lspconfig = require('lspconfig')
-
--- Configure cmp sources
-cmp.setup({
-  sources = {
-    { name = 'nvim_lsp' }, -- Enable nvim-cmp LSP source
-    { name = 'buffer' },   -- Enable buffer completion source
-    { name = 'path' },     -- Enable path completion source
-  },
-})
-
--- Set up LSP key mappings for pylsp
--- Example: Go to definition
-vim.api.nvim_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-
--- Set up LSP key mappings for clangd
--- Example: Find references
-vim.api.nvim_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-
--- Set leader key
-vim.g.mapleader = ' '
-
--- Save and continue on file
-vim.api.nvim_set_keymap('n', 'zz', ":update<CR>", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('v', '<C-J>', ":m '>+1<CR>gv=gv", { noremap = true })
-vim.api.nvim_set_keymap('v', '<C-K>', ":m '<-2<CR>gv=gv", { noremap = true })
-
--- Copy to clipboard
-vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>Y', '"+yg_', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>yy', '"+yy', { noremap = true })
--- Paste from clipboard
-vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true })
-vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true }) vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
-
-vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
-
--- My configs
-vim.cmd('filetype plugin indent on')
-vim.cmd('set autoindent')
---vim.cmd('set clipboard=unnamedplus')
-vim.cmd('set cursorline')
-vim.cmd('set expandtab')
-vim.cmd('set inccommand=split')
-vim.cmd('set number')
-vim.cmd('set noswapfile')
-vim.cmd('set rnu')
-vim.cmd('set shiftwidth=4')
-vim.cmd('set tabstop=4')
-vim.cmd('syntax on')
-
-vim.g.onedark_config = {
-  style = 'darker',
+vim.pack.add {
+	'https://github.com/neovim/nvim-lspconfig',
+	'https://github.com/stevearc/oil.nvim',
+	'https://github.com/catppuccin/nvim',
 }
---vim.cmd('colorscheme catppuccin-macchiato')
-require('onedark').load()
+
+require("oil").setup()
+vim.cmd.colorscheme("catppuccin")
